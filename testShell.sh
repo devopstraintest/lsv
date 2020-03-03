d1c58e76c9aaa3f3f17d205d379e5a0d2acdeb25
antonio <antonio-home@gmail.com>
2020-03-03 19:07:58 +0100

    ls  update

--- testShell.sh
+++ testShell.sh
@@ -2,16 +2,16 @@
 
 git add .
 git commit -m "ls $(date + '%Y-%m-%d') update "
-SHAholder=$(git cherry -v)
-git push
-
-if [ -z "$SHAholder" ]
-then
-  echo 'GERRIT_LINK_TEXT'='' >> tst2.properties
-  echo 'GERRIT_URL'='Everything up to date!' >> tst2.properties
-else
-  command=$(git log --name-status HEAD^..HEAD | grep Author | awk '{print $2}')
-  echo 'GERRIT_LINK_TEXT'='Gerrit url:' >> tst2.properties
-  echo 'GERRIT_URL'=$command >> tst2.properties
-fi
+#SHAholder=$(git cherry -v)
+#git push
+#
+#if [ -z "$SHAholder" ]
+#then
+#  echo 'GERRIT_LINK_TEXT'='' >> tst2.properties
+#  echo 'GERRIT_URL'='Everything up to date!' >> tst2.properties
+#else
+#  command=$(git log --name-status HEAD^..HEAD | grep Reviewed | awk '{print $2}')
+#  echo 'GERRIT_LINK_TEXT'='Gerrit url:' >> tst2.properties
+#  echo 'GERRIT_URL'=$command >> tst2.properties
+#fi
 
