6b984c3c5d4392f089c4849840f83ba8bc833648
antonio <antonio-home@gmail.com>
2020-03-03 13:39:49 +0100

    ls  update

--- testShell.sh
+++ testShell.sh
@@ -7,14 +7,14 @@ git push
 echo $SHAholder
 
 
-#if [ -z "$SHAholder" ]
-#then
-#  echo 'GERRIT_LINK_TEXT'=''
-#  echo 'GERRIT_URL'='Everything up to date!'
-#else
-#  command=$(git log --name-status HEAD^..HEAD | grep Author | awk '{print $2}')
-#  echo $command="test"  >> tst.properties
-#fi
+if [ -z "$SHAholder" ]
+then
+  echo 'GERRIT_LINK_TEXT'=''
+  echo 'GERRIT_URL'='Everything up to date!'
+else
+  command=$(git log --name-status HEAD^..HEAD | grep Author | awk '{print $2}')
+  echo $command="test"  >> tst.properties
+fi
 
 #key='key_value'
 #value=12345
