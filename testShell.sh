b2b4f473e1e2d8c5ebed3ea485acf52f9d6404be
antonio <antonio-home@gmail.com>
2020-03-03 19:46:13 +0100

    ls  update

--- testShell.sh
+++ testShell.sh
@@ -7,9 +7,9 @@ changes=$?
 
 echo $changes
 git commit -m "ls $(date + '%Y-%m-%d') update "
-git log @{u}.. --quiet
-changes=$?
-echo $changes
+git log @{u}.. -p
+#changes=$?
+#echo $changes
 #SHAholder=$(git cherry -v)
 #git push
 #
