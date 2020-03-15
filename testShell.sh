ab4e6bd37263bc438c53f0c9f7eba35d0aa5fe83
antonio <antonio-home@gmail.com>
2020-03-15 20:01:03 +0100

    15.3.20:00

--- testShell.sh
+++ testShell.sh
@@ -14,8 +14,8 @@ git push
 #  echo $command="test"  >> tst.properties
 #fi
 
-#key='key_value'
-#value=12345
-#
-#echo 'GERRIT_LINK_TEXT'='Gerrit url:' >> tst.properties
-#echo 'GERRIT_URL'=$value >> tst.properties
+"testing test"
+"another test"
+"another test"
+
+"another test"
