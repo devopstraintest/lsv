f923f5461ada3b2b1c736b5c21d45980349e0bc4
antonio <antonio-home@gmail.com>
2020-03-17 19:01:39 +0100

    ls $(date + '%Y-%m-%d') update

--- testShell.sh
+++ testShell.sh
@@ -1,5 +1,16 @@
-1
-2
-3
-4
-5
+#!/bin/bash
+
+changes=$(git diff --quiet)
+
+"testing test"
+
+git add .
+echo $changes
+git commit -m "date $(date + '%Y-%m-%d') update "
+
+if git push
+then
+  echo "testing test"
+else
+  echo "no"
+fi
